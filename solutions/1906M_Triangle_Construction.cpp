1f66e9d11537a332e687dba8536f08afe743fce5
rheallyc <vireonxi@gmail.com>
2021-01-11 04:59:05 +0000

    Solve 1906M: Triangle Construction

---
+++ solutions/1906M_Triangle_Construction.cpp
@@ -0,0 +1,54 @@
+// Problem: Triangle Construction
+// URL: https://codeforces.com/problemset/problem/1906/M
+// Rating: 1700
+// Tags: greedy, math
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repn(i, a, n) for (int i = a; i <= n; i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i, a, n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<LL, int> PLI;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 210000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+int n;
+LL a[N], sum, mx;
+int main() {
+	IO;
+	cin >> n;
+	repn(i, 1, n) cin >> a[i], sum += a[i], mx = max(mx, a[i]);
+	if (2 * (sum - mx) < mx) cout << sum - mx << "\n";
+	else cout << sum / 3 << "\n";
+	return 0;
+}

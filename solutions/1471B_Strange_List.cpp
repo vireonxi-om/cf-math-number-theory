6a706def81521694e44fa3883e03d1bd86aaf908
rheallyc <vireonxi@gmail.com>
2021-04-05 05:07:56 +0000

    Solve 1471B: Strange List

---
+++ solutions/1471B_Strange_List.cpp
@@ -0,0 +1,44 @@
+// Problem: Strange List
+// URL: https://codeforces.com/problemset/problem/1471/B
+// Rating: 1100
+// Tags: brute force, greedy, implementation, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, x, a[110000], cnt[110000];
+
+void solve(){
+    scanf("%d%d", &n, &x);
+    for(int i=1; i<=n; i++) cnt[i]=0;
+    int l=1e9;
+    ll sum=0;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
+        sum+=a[i];
+        int now=a[i];
+        while(now>0){
+            if(now%x==0){
+                cnt[i]++;
+                now/=x;
+            }
+            else break;
+        }
+        l=min(l, cnt[i]);
+    }
+    ll sum2=0;
+    for(int i=1; i<=n; i++){
+        if(cnt[i]!=l) sum2+=a[i];
+        else break;
+    }
+    sum*=(l+1);
+    sum+=sum2;
+    printf("%lld\n", sum);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=1; i<=t; i++) solve();
+    return 0;
+}

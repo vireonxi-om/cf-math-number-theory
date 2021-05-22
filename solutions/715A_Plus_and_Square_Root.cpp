fb09b621af51ede1c774233f5d559fd33f1f9f86
rheallyc <vireonxi@gmail.com>
2021-05-22 23:41:54 +0000

    Solve 715A: Plus and Square Root

---
+++ solutions/715A_Plus_and_Square_Root.cpp
@@ -0,0 +1,23 @@
+// Problem: Plus and Square Root
+// URL: https://codeforces.com/problemset/problem/715/A
+// Rating: 1600
+// Tags: constructive algorithms, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+ll a[110000];
+set<pair<int, int>> s;
+multiset<ll> ans;
+
+int main(){
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        if(i==1) printf("2\n");
+        else{
+            printf("%lld\n", (ll)i*(i+1)*(i+1)-i+1);
+        }
+    }
+    return 0;
+}

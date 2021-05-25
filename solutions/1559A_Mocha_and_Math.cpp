8343a5648801453923ce61a0e42f2f4a1c3641e9
rheallyc <vireonxi@gmail.com>
2021-05-25 02:41:34 +0000

    Solve 1559A: Mocha and Math

---
+++ solutions/1559A_Mocha_and_Math.cpp
@@ -0,0 +1,55 @@
+// Problem: Mocha and Math
+// URL: https://codeforces.com/problemset/problem/1559/A
+// Rating: 900
+// Tags: bitmasks, constructive algorithms, math
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 110000;
+const int M = 11000000;
+const int mod = 1000000007;
+const int inf = (int)1e9;
+const double eps = 1e-9;
+const LL INF = 1e15;
+const int MAXN = 1001000;
+
+int n;
+
+void solve(){
+    scanf("%d", &n);
+    int num, ans;
+    repn(i, 1, n) {
+        scanf("%d", &num);
+        if (i == 1) ans = num;
+        else ans = (ans & num);
+    }
+    printf("%d\n", ans);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
+

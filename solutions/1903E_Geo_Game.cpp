418c0caac39cc72fa5420ed6956d63b660cb4052
rheallyc <vireonxi@gmail.com>
2021-08-19 13:54:53 +0000

    Solve 1903E: Geo Game

---
+++ solutions/1903E_Geo_Game.cpp
@@ -0,0 +1,117 @@
+// Problem: Geo Game
+// URL: https://codeforces.com/problemset/problem/1903/E
+// Rating: 2000
+// Tags: greedy, interactive, math
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repn(i, a, n) for (int i = a; i <= n; i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i, a, n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<LL, int> PLI;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+int n, f[N];
+vector<int> v[2];
+LL sx, sy;
+
+int solve() {
+	cin >> n;
+	if (n == -1) return 1;
+	v[0].clear(); v[1].clear();
+	cin >> sx >> sy;
+	LL x, y;
+	repn(i, 1, n) {
+		f[i] = 0;
+		cin >> x >> y;
+		LL now = (x - sx) * (x - sx) + (y - sy) * (y - sy);
+		if (now % 2) v[1].pb(i);
+		else v[0].pb(i); 
+	}
+	int s0 = v[0].size(), s1 = v[1].size();
+	int tp = 0;
+	if (s1 > n / 2) tp = 1;
+	if (tp) cout << "Second\n";
+	else cout << "First\n";
+	fflush(stdout);
+	int p0 = 0, p1 = 0;
+	rep(i, 0, n) {
+		if (i % 2 == tp) {
+			while (p0 < s0 && f[v[0][p0]]) p0 ++;
+			while (p1 < s1 && f[v[1][p1]]) p1 ++;
+			if (tp) {
+				if (p0 < s0) {
+					cout << v[0][p0] << "\n";
+					fflush(stdout);
+					f[v[0][p0]] = 1;
+				}
+				else {
+					cout << v[1][p1] << "\n";
+					fflush(stdout);
+					f[v[1][p1]] = 1;
+				}
+			}
+			else {
+				if (p1 < s1) {
+					cout << v[1][p1] << "\n";
+					fflush(stdout);
+					f[v[1][p1]] = 1;
+				}
+				else {
+					cout << v[0][p0] << "\n";
+					fflush(stdout);
+					f[v[0][p0]] = 1;
+				}
+			}
+		}
+		else {
+			int num;
+			cin >> num;
+			if (num == -1) return 1;
+			f[num] = 1;
+		}
+	}
+	return 0;
+}
+
+int main() {
+//	IO;
+	int T;
+	cin >> T;
+	repn(i, 1, T) {
+		int now = solve();
+		if (now) return 0;
+	}
+	return 0;
+}

42c58c5757ae8dceba7393770295b95dadc4bbfa
rheallyc <vireonxi@gmail.com>
2021-11-13 19:40:44 +0000

    Solve 1473C: No More Inversions

---
+++ solutions/1473C_No_More_Inversions.cpp
@@ -0,0 +1,24 @@
+// Problem: No More Inversions
+// URL: https://codeforces.com/problemset/problem/1473/C
+// Rating: 1500
+// Tags: constructive algorithms, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, k, ans[210000];
+
+void solve(){
+    scanf("%d%d", &n, &k);
+    for(int i=1; i<2*k-n; i++) ans[i]=i;
+    for(int i=k; i>=2*k-n; i--) ans[i]=3*k-n-i;
+    for(int i=1; i<=k; i++) printf("%d ", ans[i]);
+    printf("\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

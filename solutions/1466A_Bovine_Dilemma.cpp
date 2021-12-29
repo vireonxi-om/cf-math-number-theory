a8f8e3b3f2dc54a4a41fc6c18cad856365fd2f12
rheallyc <vireonxi@gmail.com>
2021-12-29 22:56:34 +0000

    Solve 1466A: Bovine Dilemma

---
+++ solutions/1466A_Bovine_Dilemma.cpp
@@ -0,0 +1,28 @@
+// Problem: Bovine Dilemma
+// URL: https://codeforces.com/problemset/problem/1466/A
+// Rating: 800
+// Tags: brute force, geometry, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, a[60], cnt[100];
+
+void solve(){
+    scanf("%d", &n);
+    for(int i=0; i<100; i++) cnt[i]=0;
+    for(int i=1; i<=n; i++) scanf("%d", &a[i]);
+    for(int i=1; i<n; i++){
+        for(int j=i+1; j<=n; j++) cnt[abs(a[i]-a[j])]++;
+    }
+    int result=0;
+    for(int i=0; i<100; i++) if(cnt[i]) result++;
+    printf("%d\n", result);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

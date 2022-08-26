a9971885f83282145965ff6e70373a80a298cb82
rheallyc <vireonxi@gmail.com>
2022-08-26 15:06:31 +0000

    Solve 1768B: Quick Sort

---
+++ solutions/1768B_Quick_Sort.cpp
@@ -0,0 +1,59 @@
+// Problem: Quick Sort
+// URL: https://codeforces.com/problemset/problem/1768/B
+// Rating: 900
+// Tags: greedy, math
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 110000;
+const int M = 11000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-10;
+const long long INF = (long long)1e18;
+
+int n, k, a[N]; 
+
+void solve() {
+	cin >> n >> k;
+	int cur = 1;
+	repn(i, 1, n) {
+		cin >> a[i];
+		if (a[i] == cur) {
+			cur ++;
+		}
+	}
+	cur --;
+	cout << (n - cur + k - 1) / k << "\n";
+}
+
+int main() {
+	IO;
+	int t;
+	cin >> t;
+	repn(i, 1, t) solve();
+	return 0;
+}
+
+
+

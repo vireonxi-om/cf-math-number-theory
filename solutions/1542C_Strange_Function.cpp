70d19a196788ac26ec1cda502b8d10fb1fd73168
rheallyc <vireonxi@gmail.com>
2022-10-03 13:36:49 +0000

    Solve 1542C: Strange Function

---
+++ solutions/1542C_Strange_Function.cpp
@@ -0,0 +1,34 @@
+// Problem: Strange Function
+// URL: https://codeforces.com/problemset/problem/1542/C
+// Rating: 1600
+// Tags: math, number theory
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n;
+const ll M=1000000007;
+
+ll gcd(ll x, ll y){
+    return y==0? x: gcd(y, x%y);
+}
+
+void solve(){
+    scanf("%lld", &n);
+    ll now=2, i=2;
+    ll ans=n*2; ans%=M;
+    while(now<=n){
+        ans+=n/now;
+        ans%=M;
+        i++;
+        now=now/gcd(now, i)*i;
+    }
+    printf("%lld\n", ans);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

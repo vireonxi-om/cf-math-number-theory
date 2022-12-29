0aa13556cfd6c25ad91a37de394a94a073dff7aa
rheallyc <vireonxi@gmail.com>
2022-12-29 15:11:42 +0000

    Solve 1459A: Red-Blue Shuffle

---
+++ solutions/1459A_Red-Blue_Shuffle.cpp
@@ -0,0 +1,30 @@
+// Problem: Red-Blue Shuffle
+// URL: https://codeforces.com/problemset/problem/1459/A
+// Rating: 800
+// Tags: math, probabilities
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, a[1100], b[1100], r, bl;
+
+void solve(){
+    scanf("%d", &n);
+    r=0; bl=0;
+    for(int i=1; i<=n; i++) scanf("%1d", &a[i]);
+    for(int i=1; i<=n; i++) scanf("%1d", &b[i]);
+    for(int i=1; i<=n; i++){
+        if(a[i]>b[i]) r++;
+        else if(a[i]<b[i]) bl++;
+    }
+    if(r>bl) printf("RED\n");
+    else if(r<bl) printf("BLUE\n");
+    else printf("EQUAL\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++) solve();
+    return 0;
+}

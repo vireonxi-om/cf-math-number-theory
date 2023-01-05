d8d429cbc87351bd00fb52db4e7f47b011c935ef
rheallyc <vireonxi@gmail.com>
2023-01-05 10:35:19 +0000

    Solve 1765E: Exchange

---
+++ solutions/1765E_Exchange.cpp
@@ -0,0 +1,53 @@
+// Problem: Exchange
+// URL: https://codeforces.com/problemset/problem/1765/E
+// Rating: 1000
+// Tags: brute force, math
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+int T, a, b, n;
+int main() {
+	IO;
+	cin >> T;
+	while (T--) {
+		cin >> n >> a >> b;
+		if (a > b) cout << "1\n";
+		else {
+			int ans = (n + a - 1) / a;
+			cout << ans << "\n";
+		}
+	}
+	return 0;
+}

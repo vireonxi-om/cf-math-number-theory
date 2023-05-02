e0c1543c8829c31c56e2250468510eeaf530dbe5
rheallyc <vireonxi@gmail.com>
2023-05-02 12:38:40 +0000

    Solve 1549A: Gregor and Cryptography

---
+++ solutions/1549A_Gregor_and_Cryptography.cpp
@@ -0,0 +1,59 @@
+// Problem: Gregor and Cryptography
+// URL: https://codeforces.com/problemset/problem/1549/A
+// Rating: 800
+// Tags: math, number theory
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-9;
+const LL INF = 1e15;
+
+int p;
+
+void solve(){
+    scanf("%d", &p);
+    int a, b;
+    if(p == 5){
+        a = 2; b = 4;
+    }
+    else{
+        p--;
+        a = 2;
+        b = p/2;
+    }
+    printf("%d %d\n", a, b);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
+
+
+

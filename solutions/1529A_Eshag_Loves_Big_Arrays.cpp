fd0744f8958a4c6a06bced1119b20ff6b2ce0120
rheallyc <vireonxi@gmail.com>
2023-07-03 03:40:53 +0000

    Solve 1529A: Eshag Loves Big Arrays

---
+++ solutions/1529A_Eshag_Loves_Big_Arrays.cpp
@@ -0,0 +1,29 @@
+// Problem: Eshag Loves Big Arrays
+// URL: https://codeforces.com/problemset/problem/1529/A
+// Rating: 800
+// Tags: constructive algorithms, greedy, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, cnt[110];
+
+void solve(){
+    for(int i=1; i<=100; i++) cnt[i]=0;
+    scanf("%d", &n);
+    int num, m=110;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &num);
+        cnt[num]++;
+        m=min(m, num);
+    }
+    printf("%d\n", n-cnt[m]);
+}
+
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

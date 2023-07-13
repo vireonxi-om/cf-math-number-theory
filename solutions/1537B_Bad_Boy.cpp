612f77cdd336289baa4c98509b0ed64e33112f16
rheallyc <vireonxi@gmail.com>
2023-07-13 21:49:37 +0000

    Solve 1537B: Bad Boy

---
+++ solutions/1537B_Bad_Boy.cpp
@@ -0,0 +1,21 @@
+// Problem: Bad Boy
+// URL: https://codeforces.com/problemset/problem/1537/B
+// Rating: 900
+// Tags: constructive algorithms, greedy, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, m, i, j;
+
+void solve(){
+    scanf("%d%d%d%d", &n, &m, &i, &j);
+    printf("%d %d %d %d\n", 1, 1, n, m);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

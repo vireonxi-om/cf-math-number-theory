8fd907adad6acd76fac6784a8cf30dd4d2d0cd85
rheallyc <vireonxi@gmail.com>
2024-03-12 11:26:58 +0000

    Solve 1763A: Absolute Maximization

---
+++ solutions/1763A_Absolute_Maximization.cpp
@@ -0,0 +1,71 @@
+// Problem: Absolute Maximization
+// URL: https://codeforces.com/problemset/problem/1763/A
+// Rating: 800
+// Tags: bitmasks, constructive algorithms, greedy, math
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 110000;
+const int M = 11000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-10;
+const long long INF = (long long)1e18;
+
+int n, a[N], b[20], c[20];
+
+
+
+void solve() {
+	cin >> n;
+	repn(i, 1, n) cin >> a[i];
+	repn(i, 0, 15){ b[i] = 0; c[i] = 1; }
+	repn(i, 1, n) {
+		repn(p, 0, 15) {
+			if (a[i] % 2) {
+				b[p] = 1;
+			}
+			else c[p] = 0;
+			a[i] /= 2;
+		}
+	}
+	int num = 1, a1 = 0, a2 = 0;
+	repn(i, 0, 15) {
+		a1 += c[i] * num;
+		a2 += b[i] * num;
+		num *= 2;
+	}
+	cout << a2 - a1 << "\n";
+}
+
+int main() {
+    IO;
+    int t;
+    cin >> t;
+    repn(i, 1, t) solve();
+    return 0;
+}
+
+
+
+

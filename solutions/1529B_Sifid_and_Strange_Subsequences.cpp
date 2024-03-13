6a84c5f65b438104062ca40bc10ddea336362b0c
rheallyc <vireonxi@gmail.com>
2024-03-13 08:33:00 +0000

    Solve 1529B: Sifid and Strange Subsequences

---
+++ solutions/1529B_Sifid_and_Strange_Subsequences.cpp
@@ -0,0 +1,40 @@
+// Problem: Sifid and Strange Subsequences
+// URL: https://codeforces.com/problemset/problem/1529/B
+// Rating: 1100
+// Tags: greedy, math, sortings
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+vector<int> a, b;
+
+void solve(){
+    scanf("%d", &n);
+    a.clear(); b.clear();
+    int num;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &num);
+        if(num<=0) a.push_back(num);
+        else b.push_back(num);
+    }
+    sort(a.begin(), a.end()); sort(b.begin(), b.end());
+    int m=2e9, cnt=a.size(), ans;
+    for(int i=0; i<cnt-1; i++){
+        m=min(m, a[i+1]-a[i]);
+    }
+    if(b.empty()) ans=cnt;
+    else{
+        if((*b.begin())<=m) cnt++;
+        ans=cnt;
+    }
+    printf("%d\n", ans);
+}
+
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

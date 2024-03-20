7d8c4785a5df7b60164515d030a2fa940333a592
rheallyc <vireonxi@gmail.com>
2024-03-20 05:37:56 +0000

    Solve 1499A: Domino on Windowsill

---
+++ solutions/1499A_Domino_on_Windowsill.cpp
@@ -0,0 +1,25 @@
+// Problem: Domino on Windowsill
+// URL: https://codeforces.com/problemset/problem/1499/A
+// Rating: 800
+// Tags: combinatorics, constructive algorithms, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, k1, k2, w, b;
+
+void solve(){
+    scanf("%d%d%d%d%d", &n, &k1, &k2, &w, &b);
+    int wt=k1+k2;
+    int bt=2*n-wt;
+    wt/=2; bt/=2;
+    if(wt>=w && bt>=b) printf("YES\n");
+    else printf("NO\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

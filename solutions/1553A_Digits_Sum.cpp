933f0403fc52fe063215bf8b7af3b1e5ed89e7e9
rheallyc <vireonxi@gmail.com>
2024-06-22 11:52:36 +0000

    Solve 1553A: Digits Sum

---
+++ solutions/1553A_Digits_Sum.cpp
@@ -0,0 +1,50 @@
+// Problem: Digits Sum
+// URL: https://codeforces.com/problemset/problem/1553/A
+// Rating: 800
+// Tags: math, number theory
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 110000;
+const int M = 650;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-9;
+const LL INF = 1e15;
+
+int n;
+
+void solve() {
+    scanf("%d", &n);
+    int ans=n/10;
+    if(n%10==9) ans++;
+    printf("%d\n", ans);
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) solve();
+    return 0;
+}
+

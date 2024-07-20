e43ff1b1a510050aad89b0cb8fcfa289bd84a03d
rheallyc <vireonxi@gmail.com>
2024-07-20 05:03:42 +0000

    Solve 1765M: Minimum LCM

---
+++ solutions/1765M_Minimum_LCM.cpp
@@ -0,0 +1,61 @@
+// Problem: Minimum LCM
+// URL: https://codeforces.com/problemset/problem/1765/M
+// Rating: 1000
+// Tags: math, number theory
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 1000100;
+const int M = 5100000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-10;
+
+
+void solve() {
+    int n;
+    cin >> n;
+    bool yes =false;
+    int a, b;
+    for (int i = 2; i * i <= n; i ++) {
+        if (n % i) continue;
+        yes = true;
+        a = n / i; b = n - a;
+        break;
+    }
+    if (!yes){
+        a = 1; b = n  -1;
+    } 
+    cout << a << " " << b << "\n";
+}
+
+int main() {
+    IO;
+    int t;
+    cin >> t; 
+    rep(i, 0, t) solve();
+    return 0;
+}
+
+
+

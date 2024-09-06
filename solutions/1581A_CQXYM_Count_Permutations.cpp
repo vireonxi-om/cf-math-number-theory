c42181633718f88b2220106966422ddb8e79eb15
rheallyc <vireonxi@gmail.com>
2024-09-06 11:01:46 +0000

    Solve 1581A: CQXYM Count Permutations

---
+++ solutions/1581A_CQXYM_Count_Permutations.cpp
@@ -0,0 +1,70 @@
+// Problem: CQXYM Count Permutations
+// URL: https://codeforces.com/problemset/problem/1581/A
+// Rating: 800
+// Tags: combinatorics, math, number theory
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 210000;
+const int M = 1100000;
+const int mod = 1000000007;
+const int inf = (int)1e9;
+const double eps = 1e-9;
+const LL INF = 1e15;
+const int maxn = 100010;
+
+int n;
+LL a[N], inv2;
+
+LL quick_pow(LL x,LL n,LL m){
+    LL res = 1;
+    while(n > 0) {
+        if(n & 1) res = res * x % m;
+        x = x * x % m;
+        n >>= 1;
+    }
+    return res;
+}
+
+void solve(){
+    scanf("%d", &n);
+    n *= 2;
+    LL ans = a[n] * inv2;
+    ans %= mod;
+    printf("%lld\n", ans);
+}
+
+int main(){
+    a[0] = 1;
+    repn(i, 1, 200000) {
+        a[i] = (a[i - 1] * i) % mod;
+    }
+    inv2 = quick_pow(2, mod - 2, mod);
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
+
+
+

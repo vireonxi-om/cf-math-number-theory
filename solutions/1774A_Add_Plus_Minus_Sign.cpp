faff8d2b330c311abaacceaceea76d453307f3b0
rheallyc <vireonxi@gmail.com>
2024-09-06 07:00:34 +0000

    Solve 1774A: Add Plus Minus Sign

---
+++ solutions/1774A_Add_Plus_Minus_Sign.cpp
@@ -0,0 +1,62 @@
+// Problem: Add Plus Minus Sign
+// URL: https://codeforces.com/problemset/problem/1774/A
+// Rating: 800
+// Tags: constructive algorithms, math
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 110000;
+const int M = 110000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-10;
+const long long INF = (long long)1e18;
+
+int n;
+string s, now;
+
+void solve() {
+    cin >> n;
+    cin >> s;
+    now = "-";
+    rep(i, 1, s.length()) {
+        if (s[i] == '1'){
+            cout << now;
+            if (now == "+") now = "-";
+            else now = "+";
+        } 
+        else cout << now;
+    }
+    cout << "\n";
+}
+
+int main() {
+    IO;
+    int t;
+    cin >> t;
+    repn(i, 1, t) solve();
+    return 0;
+}
+
+
+

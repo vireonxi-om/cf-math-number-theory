6bbf5af7d618d61c19bdb9acc9fda396306c7a54
rheallyc <vireonxi@gmail.com>
2024-09-21 04:48:40 +0000

    Solve 1474B: Different Divisors

---
+++ solutions/1474B_Different_Divisors.cpp
@@ -0,0 +1,50 @@
+// Problem: Different Divisors
+// URL: https://codeforces.com/problemset/problem/1474/B
+// Rating: 1000
+// Tags: binary search, constructive algorithms, greedy, math, number theory
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int d;
+ll result;
+
+void solve(){
+    scanf("%d", &d);
+    int a, b;
+    for(int i=1+d; ; i++){
+        bool can=true;
+        for(int j=2; j<=sqrt(i); j++){
+            if(i%j==0){
+                can=false;
+                break;
+            }
+        }
+        if(can){
+            a=i;
+            break;
+        }
+    }
+    for(int i=a+d; ; i++){
+        bool can=true;
+        for(int j=2; j<=sqrt(i); j++){
+            if(i%j==0){
+                can=false;
+                break;
+            }
+        }
+        if(can){
+            b=i;
+            break;
+        }
+    }
+    result=(ll)a*b;
+    printf("%lld\n", result);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

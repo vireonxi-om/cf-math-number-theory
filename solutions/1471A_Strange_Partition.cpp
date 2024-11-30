76e4e91fcb3fcb228ec1aea5c52e88cce795dee5
rheallyc <vireonxi@gmail.com>
2024-11-30 07:01:35 +0000

    Solve 1471A: Strange Partition

---
+++ solutions/1471A_Strange_Partition.cpp
@@ -0,0 +1,29 @@
+// Problem: Strange Partition
+// URL: https://codeforces.com/problemset/problem/1471/A
+// Rating: 900
+// Tags: greedy, math, number theory
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, a[110000];
+ll x;
+
+void solve(){
+    scanf("%d%d", &n, &x);
+    ll sum1=0, sum2=0;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
+        sum1+=(a[i]+x-1)/x;
+        sum2+=a[i];
+    }
+    sum2=(sum2+x-1)/x;
+    printf("%lld %lld\n", sum2, sum1);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=1; i<=t; i++) solve();
+    return 0;
+}

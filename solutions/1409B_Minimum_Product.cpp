b658bcc18e0d85174fada57a77276fbca174aaeb
rheallyc <vireonxi@gmail.com>
2025-03-30 08:51:23 +0000

    Solve 1409B: Minimum Product

---
+++ solutions/1409B_Minimum_Product.cpp
@@ -0,0 +1,41 @@
+// Problem: Minimum Product
+// URL: https://codeforces.com/problemset/problem/1409/B
+// Rating: 1100
+// Tags: brute force, greedy, math
+// Language: C++17 (GCC 7-32)
+#include <stdio.h>
+long long a, b, x, y, n, i, j, result;
+
+void solve(void)
+{
+    scanf("%d%d%d%d%d", &a, &b, &x, &y, &n);
+    if(a+b-x-y-n>=0)
+    {
+        if(a-x>=n)
+            i=(a-n)*b;
+        else
+            i=x*(a+b-x-n);
+        if(b-y>=n)
+            j=a*(b-n);
+        else
+            j=(a+b-y-n)*y;
+        if(i>j)
+            result=j;
+        else
+            result=i;
+    }
+    else
+        result=x*y;
+    printf("%lld\n", result);
+}
+
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (;t>0;t--)
+        solve();
+    return 0;
+}
+

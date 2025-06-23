6a364070ba0f34afbc55d7a1c12a21f383448371
rheallyc <vireonxi@gmail.com>
2025-06-23 11:59:34 +0000

    Solve 1455A: Strange Functions

---
+++ solutions/1455A_Strange_Functions.cpp
@@ -0,0 +1,23 @@
+// Problem: Strange Functions
+// URL: https://codeforces.com/problemset/problem/1455/A
+// Rating: 800
+// Tags: math, number theory
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+char a[200];
+
+void solve(){
+    scanf("%s", &a);
+    printf("%d\n", strlen(a));
+}
+
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=1; i<=t; i++) solve();
+    return 0;
+}
+
+

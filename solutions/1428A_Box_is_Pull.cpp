3440307854af8f31d4db6286d54b3021207a7007
rheallyc <vireonxi@gmail.com>
2026-01-29 13:21:56 +0000

    Solve 1428A: Box is Pull

---
+++ solutions/1428A_Box_is_Pull.cpp
@@ -0,0 +1,27 @@
+// Problem: Box is Pull
+// URL: https://codeforces.com/problemset/problem/1428/A
+// Rating: 800
+// Tags: math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int x, y, a, b, c, d;
+ll result;
+
+void solve(){
+    result=0;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    x=abs(a-c);
+    y=abs(b-d);
+    if(x && y) result+=2;
+    result=result+x+y;
+    printf("%lld\n", result);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++) solve();
+    return 0;
+}

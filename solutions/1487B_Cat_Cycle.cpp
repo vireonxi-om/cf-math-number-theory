02ae93dbf2822df28d45afa0b5985d9429b9aea5
rheallyc <vireonxi@gmail.com>
2026-01-31 15:01:39 +0000

    Solve 1487B: Cat Cycle

---
+++ solutions/1487B_Cat_Cycle.cpp
@@ -0,0 +1,29 @@
+// Problem: Cat Cycle
+// URL: https://codeforces.com/problemset/problem/1487/B
+// Rating: 1200
+// Tags: math, number theory
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+ll n, k;
+
+void solve(){
+    scanf("%lld%lld", &n, &k);
+    ll tot=k;
+    if(n%2==1){
+        ll h=n/2;
+        ll t=k/h;
+        if(k%h==0) t--;
+        tot+=t;
+    }
+    if(tot%n==0) printf("%lld\n", n);
+    else printf("%lld\n", tot%n);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

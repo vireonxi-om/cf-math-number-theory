89b2760a1bf4ccadaea949855eb587ee1ee7d2f5
rheallyc <vireonxi@gmail.com>
2026-02-12 10:03:55 +0000

    Solve 1530A: Binary Decimal

---
+++ solutions/1530A_Binary_Decimal.cpp
@@ -0,0 +1,26 @@
+// Problem: Binary Decimal
+// URL: https://codeforces.com/problemset/problem/1530/A
+// Rating: 800
+// Tags: greedy, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+
+void solve(){
+    scanf("%d", &n);
+    int num=0;
+    while(n){
+        num=max(num, n%10);
+        n/=10;
+    }
+    printf("%d\n", num);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

94ec356c32a186a66b6a90bc6c9d0693d3fe0124
rheallyc <vireonxi@gmail.com>
2026-07-29 10:38:34 +0000

    Solve 1447A: Add Candies

---
+++ solutions/1447A_Add_Candies.cpp
@@ -0,0 +1,25 @@
+// Problem: Add Candies
+// URL: https://codeforces.com/problemset/problem/1447/A
+// Rating: 800
+// Tags: constructive algorithms, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+
+void solve(){
+    scanf("%d", &n);
+    printf("%d\n", n);
+    for(int i=1; i<=n; i++) printf("%d ", i);
+    printf("\n");
+}
+
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=1; i<=t; i++) solve();
+    return 0;
+}
+
+
